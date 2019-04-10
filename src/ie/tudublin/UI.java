541a564049686ee38a6f1a6d3985d5891e34e564
Seungmin Lee <Seungmin Lee>
2019-04-10 17:24:16 +0100

    lab test - 1

--- src/ie/tudublin/UI.java
+++ src/ie/tudublin/UI.java
@@ -20,18 +20,54 @@ public class UI extends PApplet
 
 	public void settings()
 	{
-		size(500, 800);
-		
+		//size(500, 800);
+		size(500, 500);
 		separate(381);
 		separate(1);
 		separate(92);
 	}
 
+
+
 	public void setup() 
 	{
+		
+
+	}
+
+	void drawRect()
+	{
+		colorMode(RGB);
+		stroke(0);
+		noFill();
+
+		float numBoxes = 4;
+		float gap = height / numBoxes;
+		for(int box = 0 ; box < numBoxes ; box ++)
+		{
+			rect(180, box * (gap-30), gap + 30, 50) ;
+		}
+	}
+
+	void drawLine()
+	{
+		stroke(0);
+
+		float numLines = 4;
+		float gap = height / numLines;
+		for(int line = 0 ; line < numLines ; line++)
+		{
+			//float x = 1 *gap;
+			line(100, line * (gap-30), 80 , 80);
+		}
+
+		
 	}
 	
 	public void draw()
-	{			
+	{		
+		drawRect();
+		drawLine();
+
 	}
 }
